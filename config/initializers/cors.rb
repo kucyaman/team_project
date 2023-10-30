1292316d3b790822b70f84b8af0185e9aa831636
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2023-10-30 18:10:28 +0900

    callback_urlの記述をcredentials.ymlからsorceryに変更

--- config/initializers/cors.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.configure do
-  config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'http://localhost:8000',
-    'Access-Control-Request-Methods' => '*',
-    'Access-Control-Request-Headers' => '*'
-  }
-end
