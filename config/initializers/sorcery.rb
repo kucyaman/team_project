c8c901215dbd60883364b30de037cf6d9e5b2490
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2023-11-02 13:20:46 +0900

    googleのコールバックURLをクレデンシャルファイルに記述

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -160,7 +160,7 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.google.key = Rails.application.credentials.dig(:google, :google_client_id)
   config.google.secret = Rails.application.credentials.dig(:google, :google_client_secret)
-  config.google.callback_url = 'https://loguma.onrender.com/oauth/callback?provider=google'
+  config.google.callback_url = Rails.application.credentials.dig(:google, :callback_url)
   config.google.user_info_mapping = {:email => "email", :remote_name => "name"}
   config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
   #
