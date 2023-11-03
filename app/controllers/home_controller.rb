f00ea7b3117e360475979d0b690b1cfc10ffaf2d
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2023-11-03 11:01:31 +0900

    ランキングの実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
     skip_before_action :require_login, only: %i[top]    
     def top
         @logs = User.fetch_user_data
-        @user_log_ranks = User.joins(:curriculum_logs).select("users.*, count(curriculum_logs.id) as curriculum_logs_count").group("users.id").order("curriculum_logs_count DESC")
+        @user_log_ranks = User.joins(:curriculum_logs).select("users.*, count(curriculum_logs) as curriculum_logs_count").group("users.id").order("curriculum_logs_count DESC")
         yesterday = Date.yesterday
         @user_study_time = User.joins(:curriculum_logs).where("curriculum_logs.created_at >= ? AND curriculum_logs.created_at < ?", yesterday.beginning_of_day, yesterday.end_of_day).group("users.id").select("users.id, SUM(curriculum_logs.hour * 60 + curriculum_logs.minutes) AS total_study_minutes").order("total_study_minutes DESC")
     end
